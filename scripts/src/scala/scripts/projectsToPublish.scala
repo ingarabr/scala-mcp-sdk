0bfcbb221f12c36f618ba0c503492a4bc09614c4
Ingar Abrahamsen <ingar@abrahams1.com>
2025-11-11 23:36:48 +0100

    chore: publish setup in bleep

---
+++ scripts/src/scala/scripts/projectsToPublish.scala
@@ -0,0 +1,14 @@
+package scripts
+
+import bleep.model
+
+object projectsToPublish {
+  // will publish these with dependencies
+  def include(crossName: model.CrossProjectName): Boolean =
+    crossName.name.value match {
+      case "schemas"       => true
+      case "server"        => true
+      case "server-http4s" => true
+      case _                 => false
+    }
+}
